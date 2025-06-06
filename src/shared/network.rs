f1a5dcb0812914f5a4e73ffc418e16a21dc86695
Lukasz Szpakowski <luckboy@vp.pl>
2025-06-06 16:16:27 +0200

    Optimized backpropagation.

--- src/shared/network.rs
+++ src/shared/network.rs
@@ -105,97 +105,85 @@ impl Net for Network
     
     fn backpropagate(&self, i: &Matrix, hs: &[Matrix], os: &[Matrix], ys: &[Matrix], one: &Matrix) -> Self
     {
-        let mut dj_dnet: Option<Network> = None;
-        for (pv_count_1, (o, y)) in os.iter().zip(ys).enumerate() {
-            let pv_count = pv_count_1 + 1;
-            let depth = hs.len() - ys.len() - 1;
-            let mut j = hs.len() - (os.len() - pv_count) - 1;
-            let dj_do = o.softmax() - y;
-            let dj_dow = &dj_do * hs[j].t();
-            let dj_dob = &dj_do * one;
-            // dj/dz = (ow^T * dj/do) (*) phi'(z)
-            let mut dj_dh = self.ow.t() * &dj_do;
-            let mut dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
-            j -= 1;
-            let (dj_dpw, dj_dpb) = if pv_count > 1 {
-                // dj/dpw = dj/dz2 * phi(z)^T
-                // dj/dpb = dj/dz2
-                let mut tmp_dj_dpw = &dj_dz * &hs[j].t();
-                let mut tmp_dj_dpb = &dj_dz * one;
-                let mut tmp = dj_dz.clone();
-                for _ in 1..pv_count {
-                    // dj/dpw += ((pw^T * dj/dz2) (*) phi'(z)) * h^T
-                    // dj/dpb += (pw^T * dj/dz2) (*) phi'(z)
-                    tmp = self.pw.t() * &tmp;
-                    tmp = tmp.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
-                    j -= 1;
-                    tmp_dj_dpw += &tmp * hs[j].t();
-                    tmp_dj_dpb += &tmp * one;
-                }
-                dj_dz = tmp;
-                (tmp_dj_dpw, tmp_dj_dpb)
-            } else {
-                let tmp_dj_dpw = &dj_dz * hs[j].t();
-                let tmp_dj_dpb = &dj_dz * one;
-                (tmp_dj_dpw, tmp_dj_dpb)
-            };
-            // dj/dz = (pw^T * dj/dz2) (*) phi'(z)
-            dj_dh = self.pw.t() * &dj_dz;
-            dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
-            j -= 1;
-            let (dj_dsw, dj_dsb) = if depth > 1 {
-                // dj/dsw = dj/dz2 * phi(z)^T
-                // dj/dsb = dj/dz2
-                let mut tmp_dj_dsw = &dj_dz * &hs[j].t();
-                let mut tmp_dj_dsb = &dj_dz * one;
-                let mut tmp = dj_dz.clone();
-                for _ in 1..depth {
-                    // dj/dsw += ((sw^T * dj/dz2) (*) phi'(z)) * h^T
-                    // dj/dsb += (sw^T * dj/dz2) (*) phi'(z)
-                    tmp = self.sw.t() * &tmp;
-                    tmp = tmp.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
-                    j -= 1;
-                    tmp_dj_dsw += &tmp * hs[j].t();
-                    tmp_dj_dsb += &tmp * one;
-                }
-                dj_dz = tmp;
-                (tmp_dj_dsw, tmp_dj_dsb)
-            } else {
-                let tmp_dj_dsw = &dj_dz * hs[j].t();
-                let tmp_dj_dsb = &dj_dz * one;
-                (tmp_dj_dsw, tmp_dj_dsb)
-            };
-            // dj/dz = (sw^T * dj/dz2) (*) phi'(z)
-            dj_dh = self.sw.t() * &dj_dz;
-            dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
-            let dj_diw = &dj_dz * i.t();
-            let dj_dib = &dj_dz * one;
-            match &mut dj_dnet {
-                Some(dj_dnet) => {
-                    dj_dnet.iw += dj_diw;
-                    dj_dnet.ib += dj_dib;
-                    dj_dnet.sw += dj_dsw;
-                    dj_dnet.sb += dj_dsb;
-                    dj_dnet.pw += dj_dpw;
-                    dj_dnet.pb += dj_dpb;
-                    dj_dnet.ow += dj_dow;
-                    dj_dnet.ob += dj_dob;
-                },
-                None => {
-                    dj_dnet = Some(Network {
-                            iw: dj_diw,
-                            ib: dj_dib,
-                            sw: dj_dsw,
-                            sb: dj_dsb,
-                            pw: dj_dpw,
-                            pb: dj_dpb,
-                            ow: dj_dow,
-                            ob: dj_dob,
-                    });
-                },
+        let pv_count = ys.len();
+        let depth = hs.len() - ys.len() - 1;
+        let mut j = hs.len() - 1;
+        let mut dj_do = os[pv_count - 1].softmax() - &ys[pv_count - 1];
+        let mut dj_dow = &dj_do * hs[hs.len() - 1].t();
+        let mut dj_dob = &dj_do * one;
+        // dj/dz = (ow^T * dj/do) (*) phi'(z)
+        let mut dj_dh = self.ow.t() * &dj_do;
+        let mut dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
+        j -= 1;
+        let (dj_dpw, dj_dpb) = if pv_count > 1 {
+            // dj/dpw = dj2/dz2 * phi(z)^T
+            // dj/dpb = dj2/dz2
+            let mut tmp_dj_dpw = &dj_dz * &hs[j].t();
+            let mut tmp_dj_dpb = &dj_dz * one;
+            let mut tmp = dj_dz.clone();
+            for k in 1..pv_count {
+                dj_do = os[pv_count - k - 1].softmax() - &ys[pv_count - k - 1];
+                dj_dow += &dj_do * hs[hs.len() - k - 1].t();
+                dj_dob += &dj_do * one;
+                // dj/dz = (ow^T * dj/do) (*) phi'(z)
+                dj_dh = self.ow.t() * &dj_do;
+                dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
+                // dj/dpw += ((pw^T * dj2/dz2) (*) phi'(z) + dj1/dz1) * h^T
+                // dj/dpb += (pw^T * dj2/dz2) (*) phi'(z) + dj1/dz1
+                tmp = self.pw.t() * &tmp;
+                tmp = tmp.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0))) + &dj_dz;
+                j -= 1;
+                tmp_dj_dpw += &tmp * hs[j].t();
+                tmp_dj_dpb += &tmp * one;
             }
+            dj_dz = tmp;
+            (tmp_dj_dpw, tmp_dj_dpb)
+        } else {
+            let tmp_dj_dpw = &dj_dz * hs[j].t();
+            let tmp_dj_dpb = &dj_dz * one;
+            (tmp_dj_dpw, tmp_dj_dpb)
+        };
+        // dj/dz = (pw^T * dj/dz2) (*) phi'(z)
+        dj_dh = self.pw.t() * &dj_dz;
+        dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
+        j -= 1;
+        let (dj_dsw, dj_dsb) = if depth > 1 {
+            // dj/dsw = dj/dz2 * phi(z)^T
+            // dj/dsb = dj/dz2
+            let mut tmp_dj_dsw = &dj_dz * &hs[j].t();
+            let mut tmp_dj_dsb = &dj_dz * one;
+            let mut tmp = dj_dz.clone();
+            for _ in 1..depth {
+                // dj/dsw += ((sw^T * dj/dz2) (*) phi'(z)) * h^T
+                // dj/dsb += (sw^T * dj/dz2) (*) phi'(z)
+                tmp = self.sw.t() * &tmp;
+                tmp = tmp.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
+                j -= 1;
+                tmp_dj_dsw += &tmp * hs[j].t();
+                tmp_dj_dsb += &tmp * one;
+            }
+            dj_dz = tmp;
+            (tmp_dj_dsw, tmp_dj_dsb)
+        } else {
+            let tmp_dj_dsw = &dj_dz * hs[j].t();
+            let tmp_dj_dsb = &dj_dz * one;
+            (tmp_dj_dsw, tmp_dj_dsb)
+        };
+        // dj/dz = (sw^T * dj/dz2) (*) phi'(z)
+        dj_dh = self.sw.t() * &dj_dz;
+        dj_dz = dj_dh.mul_elems(&(hs[j].mul_elems(&hs[j]).rsub(1.0)));
+        let dj_diw = &dj_dz * i.t();
+        let dj_dib = &dj_dz * one;
+        Network {
+            iw: dj_diw,
+            ib: dj_dib,
+            sw: dj_dsw,
+            sb: dj_dsb,
+            pw: dj_dpw,
+            pb: dj_dpb,
+            ow: dj_dow,
+            ob: dj_dob,
         }
-        dj_dnet.unwrap()
     }
 
     fn op<F>(&self, network: &Self, mut f: F) -> Self
