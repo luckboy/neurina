64d711a4d3c3a6c9d62b97ce03c45b631f940680
Lukasz Szpakowski <luckboy@vp.pl>
2025-08-12 17:44:37 +0200

    Partially added documentation.

--- src/trainer/algorithm.rs
+++ src/trainer/algorithm.rs
@@ -9,14 +9,21 @@ use std::io::Result;
 use crate::trainer::gradient_add::*;
 use crate::trainer::TrainerResult;
 
+/// A name of neural network.
 pub const NETWORK_NAME: &'static str = "neurina.nnet";
+/// A prefix of name of neural network.
 pub const NETWORK_NAME_PREFIX: &'static str = "neurina";
+/// A suffix of name of neural network.
 pub const NETWORK_NAME_SUFFIX: &'static str = ".nnet";
 
+/// A name of algorithm parameters.
 pub const PARAMS_NAME: &'static str = "params.toml";
 
+/// A name of algorithm state.
 pub const STATE_NAME: &'static str = "state.toml";
+/// A prefix of name of algorithm state.
 pub const STATE_NAME_PREFIX: &'static str = "state";
+/// A suffix of name of algorithm state.
 pub const STATE_NAME_SUFFIX: &'static str = ".toml";
 
 /// An algorithm trait.
