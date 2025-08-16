1609e8ec25047b4290d76fa494ad296a83ff3bc5
Lukasz Szpakowski <luckboy@vp.pl>
2025-08-16 12:43:01 +0200

    Added one neural searcher.

---
+++ src/engine/one_neural_searcher.rs
@@ -0,0 +1,121 @@
+//
+// Copyright (c) 2025 Łukasz Szpakowski
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+use std::sync::Arc;
+use std::sync::Mutex;
+use crate::chess::movegen::legal;
+use crate::chess::Board;
+use crate::chess::Move;
+use crate::matrix::Frontend;
+use crate::matrix::Matrix;
+use crate::engine::neural_search::*;
+use crate::shared::converter::*;
+use crate::shared::intr_check::*;
+use crate::shared::matrix_buffer::*;
+use crate::shared::net::*;
+use crate::shared::Interruption;
+
+pub struct OneNeuralSearcher<T>
+{
+    intr_checker: Arc<dyn IntrCheck + Send + Sync>,
+    converter: Converter,
+    matrix_buf: Mutex<MatrixBuffer<(Vec<f32>, Vec<Board>)>>,
+    network: T,
+}
+
+impl<T> OneNeuralSearcher<T>
+{
+    pub const MAX_COL_COUNT: usize = 1024;
+    
+    pub const MOVE_EPS: f32 = 0.01;
+    
+    pub fn new(intr_checker: Arc<dyn IntrCheck + Send + Sync>, converter: Converter, network: T) -> Self
+    {
+        let matrix_buf = Mutex::new(MatrixBuffer::new(Converter::BOARD_ROW_COUNT, 0, Self::MAX_COL_COUNT, 0, (vec![0.0; converter.move_row_count() * Self::MAX_COL_COUNT], vec![Board::initial(); Self::MAX_COL_COUNT])));
+        OneNeuralSearcher {
+            intr_checker,
+            converter,
+            matrix_buf,
+            network,
+        }
+    }
+    
+    pub fn converter(&self) -> &Converter
+    { &self.converter }
+
+    pub fn network(&self) -> &T
+    { &self.network }
+}
+
+impl<T: Net> NeuralSearch for OneNeuralSearcher<T>
+{
+    fn intr_checker(&self) -> &Arc<dyn IntrCheck + Send + Sync + 'static>
+    { &self.intr_checker }
+    
+    fn search(&self, board: &Board, pvs: &mut [Vec<Move>], depth: usize) -> Result<(), Interruption>
+    {
+        let mut matrix_buf_g = self.matrix_buf.lock().unwrap();
+        matrix_buf_g.do_elems(pvs, 0, &*self.intr_checker, |pv, elems, _, j, col_count| {
+                let mut tmp_board = board.clone();
+                for mv in pv {
+                    match tmp_board.make_move(*mv) {
+                        Ok(tmp_new_board) => tmp_board = tmp_new_board,
+                        Err(_) => break,
+                    }
+                }
+                self.converter.board_to_matrix_col(&tmp_board, elems, j, col_count);
+        }, |i, _, pair, pvs| {
+                let (output_elems, boards) = pair;
+                let col_count = pvs.len();
+                for (j, pv) in pvs.iter().enumerate() {
+                    let mut tmp_board = board.clone();
+                    for mv in pv {
+                        match tmp_board.make_move(*mv) {
+                            Ok(tmp_new_board) => tmp_board = tmp_new_board,
+                            Err(_) => break,
+                        }
+                    }
+                    boards[j] = tmp_board.clone();
+                }
+                let mut tmp_i = i.clone();
+                let mut is_first = true;
+                for _ in 0..depth {
+                    if !is_first {
+                        let mut elems = vec![0.0f32; Converter::BOARD_ROW_COUNT * col_count];
+                        for (j, board) in boards.iter().enumerate() {
+                            self.converter.board_to_matrix_col(board, elems.as_mut_slice(), j, col_count);
+                        }
+                        tmp_i = Matrix::new_with_elems(Converter::BOARD_ROW_COUNT, col_count, elems.as_slice());
+                    }
+                    self.network.compute(&tmp_i, 1, 1, |_| self.intr_checker.check(), |o| {
+                            self.intr_checker.check()?;
+                            let frontend = Frontend::new().unwrap();
+                            let mut is_transposed = false;
+                            frontend.get_elems_and_transpose_flag(&o, &mut output_elems[0..(self.converter.move_row_count() * col_count)], &mut is_transposed).unwrap();
+                            for (j, (pv, board)) in pvs.iter_mut().zip(boards.iter_mut()).enumerate() {
+                                let moves = legal::gen_all(board);
+                                match self.converter.matrix_col_to_move(&moves, board.side(), output_elems.as_slice(), j, col_count, Self::MOVE_EPS) {
+                                    Some(mv) => {
+                                        match board.make_move(mv) {
+                                            Ok(new_board) => {
+                                                pv.push(mv);
+                                                *board = new_board;
+                                            },
+                                            Err(_) => (),
+                                        }
+                                    },
+                                    None => (),
+                                }
+                            }
+                            Ok(())
+                    })?;
+                    is_first = false;
+                }
+                Ok(())
+        })
+    }
+}
